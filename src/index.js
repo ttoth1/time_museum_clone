f2fd85d12a2a11018403fb8b2b74cda8a50cac37
Trevor Toth <trevor.toth11@gmail.com>
2021-12-08 21:22:44 -0500

    auto

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import EndScreen from "./EndScreen.js"
 import GameScene3 from "./GameScene3.js";
 
 var config = {
-  type: Phaser.CANVAS,
+  type: Phaser.AUTO,
   parent: "phaser-example",
   width: 800,
   height: 600,
